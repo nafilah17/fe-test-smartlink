2c76fa9d30d581fd2cd87af0770b265acb0058cc
nafilah17 <rissanafilah@gmail.com>
2022-03-24 13:28:30 +0700

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Smartlink-Gaji</a>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
